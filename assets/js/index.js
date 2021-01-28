b8d0a44bcf2c6392511eed00dbc4f7a77cf1556c
Ivan Khomenko <Simkav2411@gmail.com>
2021-01-28 19:58:34 +0200

    Add task form solution

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,116 +1,28 @@
 'use strict';
 
-const cardContainer = document.getElementById('root');
-const cards = data.map((place) => createPlaceCards(place));
-cardContainer.append(...cards);
+const arr = [];
+const ulList = document.getElementById('root-list');
 
-function createPlaceCards(place) {
-  const p = createElement(
-    'p',
-    { classNames: ['cardDescription'] },
-    document.createTextNode(place.description)
-  );
+const form = document.getElementById('root-form');
 
-  const h3 = createElement(
-    'h3',
-    { classNames: ['cardName'] },
-    document.createTextNode(place.name || '')
-  );
-
-  const article = createElement(
-    'article',
-    { classNames: ['cardContainer'] },
-    createImageWrapper(place),
-    h3,
-    p
-  );
-
-  return createElement('li', { classNames: ['cardWrapper'] }, article);
-}
-
-function createImageWrapper(place) {
-  const { name, id } = place;
-
-  const imageWrapper = document.createElement('div');
-  imageWrapper.setAttribute('id', `wrapper${id}`);
-  imageWrapper.classList.add('cardImageWrapper');
-  imageWrapper.style.backgroundColor = stringToColour(name);
-
-  const initials = document.createElement('div');
-  initials.classList.add('initials');
-  initials.append(document.createTextNode(name.trim().charAt(0) || ''));
-
-  createImage(place, { className: 'cardImage' });
-
-  imageWrapper.append(initials);
-  return imageWrapper;
-}
-
-function createImage({ name, profilePicture, id }, { className }) {
-  const img = document.createElement('img');
-  img.classList.add(className);
-  img.dataset.id = id;
-  img.setAttribute('alt', name);
-  img.setAttribute('src', profilePicture);
-  img.addEventListener('error', handleImageError);
-  img.addEventListener('load', handleImageLoad);
-  return img;
-}
- */
-// btn.addEventListener('click', btnHandler);
-// btn.dispatchEvent(new MouseEvent('click'));
-
-/* 
-  По нажатию на кнопку - отобразить содержимое этой кнопки
-*/
-
-/**
- *
- * @param {string} type
- * @param {object} options
- * @param {string[]} options.classNames - css classes
- * @param {function} options.onClick - click handler
- * @param {Node[]} children
- * @return {HTMLElement}
- */
-function createElement(type, { classNames, onClick }, ...children) {
-  const elem = document.createElement(type);
-  elem.classList.add(...classNames);
-  elem.onclick = onClick;
-  elem.append(...children);
-  return elem;
-}
-
-/* 
-  EVENT HANDLERS
-*/
-
-function handleImageError({ target }) {
-  target.remove();
-}
-
-function handleImageLoad({
-  target,
-  target: {
-    dataset: { id },
-  },
-}) {
-  document.getElementById(`wrapper${id}`).append(target);
-}
-
-/* 
-  UTILS
-*/
-
-function stringToColour(str) {
-  let hash = 0;
-  for (let i = 0; i < str.length; i++) {
-    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+form.addEventListener('submit', (e) => {
+  const {
+    target: {
+      body,
+      body: { value: val },
+    },
+  } = e;
+  e.preventDefault();
+  if (val) {
+    arr.push(val);
+    ulList.append(createLiElement(val));
+    body.value = '';
+  } else {
+    console.log('Empty input');
   }
-  let colour = '#';
-  for (let i = 0; i < 3; i++) {
-    let value = (hash >> (i * 8)) & 0xff;
-    colour += ('00' + value.toString(16)).substr(-2);
-  }
-  return colour;
-}
+});
+const createLiElement = (textNode) => {
+  const liElem = document.createElement('li');
+  liElem.append(document.createTextNode(textNode));
+  return liElem;
+};
