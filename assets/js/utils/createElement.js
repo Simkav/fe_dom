89bec16a5e400f8d159d628690a6f4abdbc20d33
Ivan Khomenko <Simkav2411@gmail.com>
2021-02-04 11:04:25 +0200

    Refactor createElement

--- assets/js/utils/createElement.js
+++ assets/js/utils/createElement.js
@@ -5,12 +5,13 @@
  * @param {string[]} options.classNames - css classes
  * @param {function} options.onClick - click handler
  * @param {Map[]} options.attr - html map presents as attr,value
+ * @param {string} options.id - element id
  * @param {Node[]} children
  * @return {HTMLElement}
  */
 function createElement(
   type,
-  { attr = new Map(), classNames, onClick },
+  { attr = new Map(), classNames, onClick, id },
   ...children
 ) {
   const elem = document.createElement(type);
@@ -20,5 +21,6 @@ function createElement(
   elem.classList.add(...classNames);
   elem.onclick = onClick;
   elem.append(...children);
+  if (id) elem.id = id;
   return elem;
 }
